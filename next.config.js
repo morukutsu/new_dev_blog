e42d5c10ca92652a04a2fb392e608ad7916ddc51
Morukutsu <ulderic.k@free.fr>
2018-12-13 01:32:40 +0100

    Fix build: updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,17 @@
-const withCSS = require("@zeit/next-css");
-module.exports = withCSS();
+const { PHASE_PRODUCTION_SERVER } =
+    process.env.NODE_ENV === "development"
+        ? {}
+        : !process.env.NOW_REGION
+            ? require("next/constants")
+            : require("next-server/constants");
+
+module.exports = (phase, { defaultConfig }) => {
+    if (phase === PHASE_PRODUCTION_SERVER) {
+        // Config used to run in production.
+        return {};
+    }
+
+    const withCSS = require("@zeit/next-css");
+
+    return withCSS();
+};
